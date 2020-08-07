f6be1ce013cbdc0159d97844b7f32d79ca0abcb9
RavjotSandhu <nand2072@gmail.com>
2020-08-07 17:25:09 +0530

    update js code

---
+++ ext.js
@@ -0,0 +1,13 @@
+browser.webRequest.onBeforeRequest.addListener(
+    function(details) { 
+      // console.log(details)
+      return {cancel: true}; 
+    },
+    {urls: 
+      ["*://*.doubleclick.net/*",
+      "*://*.googleadservices.com/*",
+      "*://*.googlesyndication.com/*",
+      "*://*.moat.com/*"]
+    },
+    ["blocking"]
+  );
